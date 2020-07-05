3fdd01fbed0bf714313c1d4bf45454da8165b04a
GaoYYYang <carlsirce@126.com>
2020-07-05 23:32:41 +0800

    Create node.js.yml

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,17 +1,11 @@
-import {
-  compile,
-  getCompiler,
-  getErrors,
-  getWarnings,
-  readAsset,
-} from './helpers';
+import { compile, getCompiler, getErrors, getWarnings, readAsset } from './helpers';
 
 test('loader', async () => {
-  // it('should work', async () => {
   const compiler = getCompiler('index.js');
   const stats = await compile(compiler);
   expect(readAsset('main.bundle.js', compiler, stats)).toBeTruthy();
+  console.log(getErrors(stats));
+  console.log(getWarnings(stats));
   expect(getErrors(stats).length).toBe(0);
   expect(getWarnings(stats).length).toBe(0);
-  // });
 });
